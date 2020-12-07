b8491ac16c55d443c9ee45eed213510c62c72823
Widyaputriheso <62939137+Widyaputriheso@users.noreply.github.com>
2020-12-07 18:59:18 +0700

    Add project files.

---
+++ RentalKendaraan_040/Models/Upload/IFormFileExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+
+
+
+namespace RentalKendaraan_040.Models.Upload
+{
+	public static class IFormFileExtensions
+    {
+        public static string GetFilename(this IFormFile file)
+        {
+            return ContentDispositionHeaderValue.Parse(
+                            file.ContentDisposition).FileName.ToString().Trim('"');
+        }
+
+        public static async Task<MemoryStream> GetFileStream(this IFormFile file)
+        {
+            MemoryStream filestream = new MemoryStream();
+            await file.CopyToAsync(filestream);
+            return filestream;
+        }
+
+        public static async Task<byte[]> GetFileArray(this IFormFile file)
+        {
+            MemoryStream filestream = new MemoryStream();
+            await file.CopyToAsync(filestream);
+            return filestream.ToArray();
+
+        }
+    }
+}
