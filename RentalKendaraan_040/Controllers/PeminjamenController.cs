6d54fde9470ea6c51d51a0d117050216f04d06de
Widyaputriheso <62939137+Widyaputriheso@users.noreply.github.com>
2020-12-14 16:39:04 +0700

    ADD ROLE BASED AUTHORIZATION

--- RentalKendaraan_040/Controllers/PeminjamenController.cs
+++ RentalKendaraan_040/Controllers/PeminjamenController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RentalKendaraan_040.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 
 namespace RentalKendaraan_040.Controllers
 {
@@ -17,7 +19,7 @@ namespace RentalKendaraan_040.Controllers
         {
             _context = context;
         }
-
+        [Authorize(Policy = "readonlypolicy")]
         // GET: Peminjamen
         public async Task<IActionResult> Index(string ktsd, string searchString, string sortOrder, string currentFilter, int? pageNumber)
         {
@@ -110,6 +112,7 @@ namespace RentalKendaraan_040.Controllers
             return View(peminjaman);
         }
 
+        [Authorize(Policy = "writepolicy")]
         // GET: Peminjamen/Create
         public IActionResult Create()
         {
@@ -137,7 +140,7 @@ namespace RentalKendaraan_040.Controllers
             ViewData["IdKendaraan"] = new SelectList(_context.Kendaraan, "IdKendaraan", "NamaKendaraan", peminjaman.IdKendaraan);
             return View(peminjaman);
         }
-
+        [Authorize(Policy = "editpolicy")]
         // GET: Peminjamen/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
@@ -195,6 +198,7 @@ namespace RentalKendaraan_040.Controllers
             return View(peminjaman);
         }
 
+        [Authorize(Policy ="deletepolicy")]
         // GET: Peminjamen/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
--- RentalKendaraan_040/Startup.cs
+++ RentalKendaraan_040/Startup.cs
@@ -13,6 +13,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace RentalKendaraan_040
 {
@@ -38,6 +39,16 @@ namespace RentalKendaraan_040
 			services.AddDbContext<Models.RentKendaraanContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 			services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultUI().AddEntityFrameworkStores<RentKendaraanContext>().AddDefaultTokenProviders();
+			services.AddAuthorization(options =>
+			{
+				options.AddPolicy("readonlypolicy", builder => builder.RequireRole("Admin", "Manager", "Kasir"));
+				options.AddPolicy("writepolicy", builder => builder.RequireRole("Admin", "Kasir"));
+				options.AddPolicy("editpolicy", builder => builder.RequireRole("Admin", "Kasir"));
+				options.AddPolicy("deletepolicy", builder => builder.RequireRole("Admin", "Kasir"));
+			});
+			services.AddScoped<Peminjaman>();
+			services.AddScoped<Pengembalian>();
+
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
